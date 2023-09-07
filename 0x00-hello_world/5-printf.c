f73d13676ecabf80c8f909b2d214cf4844eb237e
Mohammed12Khair <mohammed12kh@hotmail.com>
2023-09-07 12:18:54 -0700

    test

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/**
+ *main - this function will jsut print 
+ *Return:0 mens its ok
+ *
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
