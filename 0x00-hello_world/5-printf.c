b8525a7e8933073c13d40dd7a954c72f921192df
Mohammed12Khair <mohammed12kh@hotmail.com>
2023-09-07 12:28:29 -0700

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,9 @@
 /**
  * main - this function will jsut print 
  * Return:0 mens its ok
- *
- *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
